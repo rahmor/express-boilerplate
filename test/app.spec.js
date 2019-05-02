8a7ce072a279ca7d3ef6212822d8d91f24c466d5
rahmor <rahmor@yahoo.com>
2019-05-02 13:40:23 -0400

    add require supertest

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 const app = require('../src/app');
-
+const supertest = require('supertest');
 describe('App', () => {
   it('GET / responds with 200 containing "Hello, world!"', () => {
     return supertest(app)
